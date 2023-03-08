dc08f367b9955b7d813fb07e5253be4afffd4ab5
Nam Yechan <chanmias0318@gmail.com>
2023-03-08 22:24:32 +0900

    Update main screen

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -12,20 +12,65 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         appBar: AppBar(),
         body: Column(
           children: [
+            const SizedBox(
+              height: 60,
+            ),
             Row(
               children: const [],
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Column(
                   children: const [
-                    Text('Today'),
+                    Text(
+                      'Today',
+                      style: TextStyle(
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      '50,000',
+                      style: TextStyle(
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    )
                   ],
                 ),
-                //row + 1 (month)
+                Column(
+                  children: const [
+                    Text(
+                      'Today',
+                      style: TextStyle(
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      '50,000',
+                      style: TextStyle(
+                        fontSize: 55,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
             //list box
